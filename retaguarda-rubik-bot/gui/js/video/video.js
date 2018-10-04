18fc91c55754610889217c20e526ce8cfd356c95
Andre Angelucci <andreb.angelucci@gmail.com>
2018-10-03 22:24:25 -0300

    processo de amostragem das pecas do cubo
    atraves da captura da imagem da webcam

--- retaguarda-rubik-bot/gui/js/video/video.js
+++ retaguarda-rubik-bot/gui/js/video/video.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
 const video = document.querySelector('#videoElement');
 const canvasMarcacoes = document.getElementById('canvasMarcacoes')
 const canvasSnapshot = document.getElementById('canvasSnapshot')
+const canvasPeca = document.getElementById('canvasPeca')
 const ctx = canvasMarcacoes.getContext("2d");
+
+//vetores x,y com a posicao de cada peca
+//width: 50 heigth: 50
+const arrPecas = [
+  [120,  50], [175,  50], [230,  50],
+  [120, 105], [175, 105], [230, 105],
+  [120, 160], [175, 160], [230, 160]
+]
 
 if (navigator.mediaDevices.getUserMedia) {       
     navigator.mediaDevices.getUserMedia({video: true})
@@ -19,25 +29,28 @@ function desenhaMarcacoes(){
   ctx.strokeStyle = '#00FF00';
   ctx.lineWidth = 2;
   //desenha as marcacoes para posicionar o cubo
-  ctx.strokeRect(120, 50, 50, 50)
-  ctx.strokeRect(175, 50, 50, 50)
-  ctx.strokeRect(230, 50, 50, 50)
-  //2 linha
-  ctx.strokeRect(120, 105, 50, 50)
-  ctx.strokeRect(175, 105, 50, 50)
-  ctx.strokeRect(230, 105, 50, 50)
-  //3 linha
-  ctx.strokeRect(120, 160, 50, 50)
-  ctx.strokeRect(175, 160, 50, 50)
-  ctx.strokeRect(230, 160, 50, 50)
+  for (i in arrPecas){    
+    ctx.strokeRect(arrPecas[i][0], arrPecas[i][1], 50, 50)  
+  }
 }
 
 function capturaImagens(){  
+  //salva as fotos do cubo
   canvasSnapshot.getContext('2d')
     .drawImage(video, 0, 0, 400, 300);
-  var p1 = canvasSnapshot.getContext('2d').getImageData(120, 50, 50, 50)
-  console.log(p1)
-  ctx.putImageData(p1, 50, 50)
+  for (i in arrPecas){
+    canvasPeca.getContext('2d').putImageData(
+      canvasSnapshot.getContext('2d').getImageData(arrPecas[i][0], arrPecas[i][1], 50, 50),
+      0, 0
+    );
+    var imgBuffer = new Buffer(
+      canvasPeca.toDataURL().replace(/^data:image\/\w+;base64,/, ""),
+      'base64'
+    )
+    fs.writeFile('./imgs-cubo/p'+ i+ '.png', imgBuffer, function(err){
+      console.log('Falha ao salvar imagem: '+err)
+    })
+  }  
 }
 
 function keyDown(btn){
