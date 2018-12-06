6a524883a2e7bd6b0b6343257fe0ad27ac89a0fd
Andre Angelucci <andreb.angelucci@gmail.com>
2018-12-06 00:47:24 -0200

    melhorias na apresentacao

--- apresentacao/banca/cronometro.js
+++ apresentacao/banca/cronometro.js
@@ -7,14 +7,9 @@ function onClickCronometro(tecla){
     if (!executando){
         mili = 0;
         segundo = 0;        
-        document.querySelector("#sessaoCronometro").style.backgroundColor = "#FF0000";
         func = setInterval(timer, 55);
     } else {
         clearInterval(func);
-        document.querySelector("#mensagemCronometro").innerHTML = "Cubo-Mágico finalizado!";
-        document.querySelector("#sessaoCronometro").setAttribute(
-            "background-color", "transparent"
-        );
     }
     executando = !executando;
 }
